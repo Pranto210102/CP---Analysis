2be3a1edf7097f566fd123942068467e64ba26b5
Pranto Bala <128365505+Pranto210102@users.noreply.github.com>
2025-04-29 15:48:08 +0600

    Update 0-1 BFS.cpp

--- Graph/Shortest Path/0-1 BFS.cpp
+++ Graph/Shortest Path/0-1 BFS.cpp
@@ -1,52 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n,m;
-vector<pair<int,int>>g[100100];
-int dis[100100];
+vector<vector<pair<int,int>>>g;
+vector<int>dis;
 
 void BFS01(int sc){
     deque<int>dq;
-    for(int i=1;i<=n;i++){
-        dis[i]=1e9;
-    }
-
     dis[sc]=0;
     dq.push_back(sc);
+
     while(!dq.empty()){
-         int xx = dq.front();
-         dq.pop_front();
+        int xx = dq.front();
+        dq.pop_front();
 
-         for(auto v : g[xx]){
-            int neigh = v.first;
-            int weigh = v.second;
+        for(auto v : g[xx]){
+            int nei = v.first;
+            int wei = v.second;
 
-            if(dis[neigh]>dis[xx]){
-                dis[neigh]= dis[xx] + weigh;
+            if(dis[nei] > dis[xx] + wei){
+                 dis[nei] = dis[xx] + wei;
 
-                if(weigh==0){
-                    dq.push_front(neigh);
-                }else{
-                    dq.push_back(neigh);
-                }
+                 if(wei ==0){
+                    dq.push_front(nei);
+                 }else{
+                    dq.push_back(nei);
+                 }
             }
-         }
+        }
     }
 }
 
+void solve(){
+    cin>>n>>m;
+    g.clear();
+    g.resize(n+1);
+    for(int i=1;i<=m;i++){
+        int x,y;
+        cin>>x>>y;
+        g[x].push_back({y, 0});
+        g[y].push_back({x, 1});
+    }
+    dis.assign(n+1, 1e9);
+
+    BFS01(1);
+
+    if(dis[n]>=1e9) dis[n]=-1;
+
+    cout<<dis[n]<<"\n";
+}
 
 int main(){
-    cin>>n>>m;
-    for(int i=1;i<=m;i++){
-        int a,b,c;
-        cin>>a>>b>>c;
-        g[a].push_back({b, c});
-        g[b].push_back({a, c});
-    }
-
-    int sc;
-    cin>>sc;
-
-    BFS01(sc);
-
-    // sc to all node sortest path
+    int t;
+    cin>>t;
+    while(t--)
+    solve();
 }
