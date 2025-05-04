d8836eee00cb0672a29ae7b3884446d3afdab20e
Pranto Bala <128365505+Pranto210102@users.noreply.github.com>
2025-05-05 00:03:56 +0600

    add reverse method rolling_hash.cpp

--- string_algo/rolling_hash.cpp
+++ string_algo/rolling_hash.cpp
@@ -4,7 +4,7 @@ using ll = long long;
 
 struct hasher{
     ll base,mod;
-    vector<ll>hash, basepow;
+    vector<ll>hash, basepow,revhash;
     hasher(){
 
     }
@@ -18,20 +18,36 @@ struct hasher{
             hash[i] = ((hash[i-1]*base)%mod + (s[i-1]-'a' +1))%mod;
             basepow[i] = (basepow[i-1]*base) %mod;
         }
+
+        // reverse
+        revhash.resize(n+1);
+        revhash[n]=0;
+        for(ll i=n-1;i>=0;i--){
+            revhash[i] = ((revhash[i+1]*base)%mod + (s[i]-'a' +1))%mod;
+        }
     }
 
     ll gethash(ll l, ll r){ // 0 indexed..O(1)
          ll ans = (hash[r+1]-(hash[l]*basepow[r-l+1])%mod)%mod;
          return (ans + mod)%mod;
     }
+    // reverse hash
+    ll getrevhash(ll l, ll r) {
+        ll ans = (revhash[l] - (revhash[r + 1] * basepow[r - l + 1]) % mod + mod) % mod;
+        return ans;
+    }
 };
 struct doublehasher{
      hasher h1, h2;
+     doublehasher(){}
      doublehasher(string s){
          h1 = hasher(s, 37, 999999929LL);
          h2 = hasher(s, 41, 999999937LL);
      }
      pair<ll,ll>gethash(ll l, ll r){
         return {h1.gethash(l, r), h2.gethash(l, r)};
+     }
+     pair<ll,ll>getrevhash(ll l, ll r){
+        return {h1.getrevhash(l, r), h2.getrevhash(l, r)};
      }
 };
